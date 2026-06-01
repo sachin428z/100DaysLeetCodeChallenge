d663f00757f9bbe4d79df4fbfb827e7708d750b3
Sachin <sachin3434zzz@gmail.com>
2026-06-01 22:02:44 +0530

    Time: 0 ms (100%), Space: 19.2 MB (80.96%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> depend(numCourses,vector<int>(numCourses,0));
-        unordered_map<int,int> freq;
+        vector<vector<int>> adj(numCourses);
+        vector<int> indegree(numCourses,0);
 
-        for(int i=0; i<prerequisites.size(); i++){
-            depend[prerequisites[i][1]][prerequisites[i][0]] = 1;
+        for(auto& pre: prerequisites) {
+            int a=pre[0],b=pre[1];
+            adj[b].push_back(a);
+            indegree[a]++;
         }
-        vector<int> keyRow;
-        for(int i=0; i<numCourses; i++){
-            int f = 0;
-            for(int j=0; j<numCourses; j++){
-                if(depend[i][j] == 1)f++;
-            }
-            if(f == 0)keyRow.push_back(i);
-            freq[i] = f;
-        }
-
-       for(int i=0; i<keyRow.size(); i++){
-        int m = keyRow[i];
-        for(int j=0; j<depend.size(); j++){
-            if(depend[j][m] == 1){
-                depend[j][m] = 0;
-                if(freq[j] == 1)keyRow.push_back(j);
-                freq[j]--;
+        queue<int> q;
+        for(int i=0;i<numCourses;i++) {
+            if(indegree[i]==0) {
+                q.push(i);
             }
         }
-       }
+        int count=0;
+        while(!q.empty()) {
+            int node=q.front();
+            q.pop();
+            count++;
 
-       for(int i=0; i<depend.size(); i++){
-        if(freq[i] > 0)return false;
-       }
-     return true;
+            for(int nei:adj[node]) {
+                indegree[nei]--;
+               if(indegree[nei]==0) {
+                q.push(nei);
+               }
+            }
+        }
+        return count == numCourses;
     }
 };
