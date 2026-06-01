a42da4cf17352dfada0fea5b25adf7ebc9979a41
Sachin <sachin3434zzz@gmail.com>
2026-06-01 11:04:56 +0530

    Time: 13 ms (76.25%), Space: 38.9 MB (20.42%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,25 +1,12 @@
 class Solution {
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n = mat.size();
-        int m = mat[0].size();
+    void bfs(queue<pair<pair<int,int>,int>>& q,
+             vector<vector<int>>& vis,
+             vector<vector<int>>& dist,
+             int n, int m) {
 
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-        vector<vector<int>> dist(n, vector<int>(m, 0));
-
-        queue<pair<pair<int,int>,int>> q;
-
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                if(mat[i][j] == 0) {
-                    q.push({{i,j},0});
-                    vis[i][j] = 1;
-                }
-            }
-        }
-
-        int delrow[4] = {-1,0,1,0};
-        int delcol[4] = {0,1,0,-1};
+        int delrow[4] = {-1, 0, 1, 0};
+        int delcol[4] = {0, 1, 0, -1};
 
         while(!q.empty()) {
             int row = q.front().first.first;
@@ -38,10 +25,31 @@ public:
                    vis[nrow][ncol] == 0) {
 
                     vis[nrow][ncol] = 1;
-                    q.push({{nrow,ncol}, steps + 1});
+                    q.push({{nrow, ncol}, steps + 1});
                 }
             }
         }
+    }
+
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        vector<vector<int>> dist(n, vector<int>(m, 0));
+
+        queue<pair<pair<int,int>,int>> q;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(mat[i][j] == 0) {
+                    q.push({{i, j}, 0});
+                    vis[i][j] = 1;
+                }
+            }
+        }
+
+        bfs(q, vis, dist, n, m);
 
         return dist;
     }
