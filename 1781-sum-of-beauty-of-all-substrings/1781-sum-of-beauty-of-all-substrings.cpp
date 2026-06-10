3e19906bf53b89434831e8b2de01af99b9f3c3c9
Sachin <sachin3434zzz@gmail.com>
2026-06-10 11:24:04 +0530

    Time: 126 ms (25.12%), Space: 26.2 MB (24.17%) - LeetHub

---
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int beautySum(string s) {
+        int n=s.length();
+        int sum=0;
+
+        for(int i=0;i<n;i++) {
+            unordered_map<char,int> freq;
+            for(int j=i;j<n;j++) {
+                freq[s[j]]++;
+
+                int maxi=INT_MIN;
+                int mini=INT_MAX;
+                for(auto it:freq) {
+                    maxi=max(maxi,it.second);
+                    mini=min(mini,it.second);
+                }
+                sum+=(maxi-mini);
+            }
+        }
+    return sum;
+
+    }
+};
