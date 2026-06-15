5d69870b8199957bc3e4514c27602f05d6916910
Sachin <sachin3434zzz@gmail.com>
2026-06-15 09:04:08 +0530

    Time: 0 ms (100%), Space: 11.8 MB (50.65%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
@@ -1,16 +1,30 @@
 class Solution {
-public:
-    int arithmeticTriplets(vector<int>& nums, int diff) {
-        int count=0;
-        int n=nums.size();
-        
-        for(int i=0;i<n;i++) {
-            for(int j=i+1;j<n;j++) {
-                for(int k=0;k<n;k++) {
-                    if(nums[k]-nums[j]==diff && nums[j]-nums[i]==diff) count++;
-                }
+    private:
+    bool binarySearch(vector<int>&nums, int target){
+        int low = 0;
+        int high = nums.size() - 1;
+        while (low <= high){
+            int mid = low + (high - low)/2;
+            if (nums[mid] == target){
+                return true;
+            }
+            else if (nums[mid] < target){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
             }
         }
-        return count;
+        return false;
+    }
+public:
+    int arithmeticTriplets(vector<int>& nums, int diff) {
+        int counter = 0;
+        for (int i = 0; i < nums.size(); i++){
+            if(binarySearch(nums,nums[i] + diff) && binarySearch(nums, nums[i] + (2*diff))){
+                counter ++;
+            }
+        }
+        return counter;
     }
 };
