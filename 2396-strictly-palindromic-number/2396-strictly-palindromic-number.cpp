58e853aced50ac02d901ead0920438d3c76f8df4
Sachin <sachin3434zzz@gmail.com>
2026-06-25 17:59:38 +0530

    Time: 0 ms (100%), Space: 8.1 MB (10.33%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
@@ -1,6 +1,35 @@
 class Solution {
-public:
-    bool isStrictlyPalindromic(int n) {
+public: 
+    bool palindromicNumber(string& s) {
+        int l=0;
+        int r=s.size()-1;
+        while(l<=r) {
+            if(s[l]!=s[r]) return false;
+            l++;
+            r--;
+        }
         return false;
     }
+
+    bool isStrictlyPalindromic(int n) {
+        for(int i=2;i<=n-2;i++) {
+            string s="";
+            int x=n;
+            while(x) {
+                s+=to_string(x%i);
+                x=x/i;
+            }
+            reverse(s.begin(),s.end());
+            if(!palindromicNumber(s)) return false;
+        }
+        return true;
+    }
 };
+
+
+// class Solution {
+// public:
+//     bool isStrictlyPalindromic(int n) {
+//         return false;
+//     }
+// };
