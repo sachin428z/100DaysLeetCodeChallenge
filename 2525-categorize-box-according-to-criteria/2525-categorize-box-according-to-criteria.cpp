96368c1b52c15c58d76d9cf86ca89c3c88177aaa
Sachin <sachin3434zzz@gmail.com>
2026-06-25 11:55:14 +0530

    Time: 0 ms (100%), Space: 7.9 MB (33.48%) - LeetHub

---
+++ 2525-categorize-box-according-to-criteria/2525-categorize-box-according-to-criteria.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string categorizeBox(int length, int width, int height, int mass) {
+        long long v=1LL*length*width*height;
+        int b=0,h=0;
+        
+        
+        if (length >= 10000 || width >= 10000 || height >= 10000 || v >= 1000000000)
+            b = 1;
+
+        if (mass >= 100)
+            h = 1;
+
+         if (b == 1 && h == 1)
+            return "Both";
+        else if (b == 1)
+            return "Bulky";
+        else if (h == 1)
+            return "Heavy";
+        else
+            return "Neither";
+    }
+};
