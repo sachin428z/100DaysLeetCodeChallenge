e03015ac382cb6a3cbbf148c91b63c9899284ff1
Sachin <sachin3434zzz@gmail.com>
2026-06-26 11:07:08 +0530

    Time: 3 ms (10.22%), Space: 14.6 MB (8.41%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        stack<string> st;
+        int n=logs.size();
+
+        for(int i=0;i<n;i++) {
+            if(logs[i]=="../") {
+                if(!st.empty()) st.pop();
+            } 
+            else if(logs[i]=="./") continue;
+            else st.push(logs[i]);
+        }
+        return st.size();
+    }
+};
