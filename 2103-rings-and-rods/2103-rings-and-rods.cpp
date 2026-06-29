14cb2d80ee977a5202c0ae23ab6839a1724fe709
Sachin <sachin3434zzz@gmail.com>
2026-06-29 19:34:10 +0530

    Time: 0 ms (100%), Space: 9.2 MB (11.37%) - LeetHub

--- 2103-rings-and-rods/2103-rings-and-rods.cpp
+++ 2103-rings-and-rods/2103-rings-and-rods.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int countPoints(string rings) {
         int n=rings.size();
-        unordered_map<int,unordered_set<char>> mp;
+        unordered_map<int,unordered_set<char>> mp; // array of unique colors
 
         for(int i=0;i<n;i+=2) {
             char colors=rings[i];
-            int rod=rings[i+1]-'0';
+            int rod=rings[i+1]-'0'; // char ko int me convert kr bsdk
 
             mp[rod].insert(colors);
         }
