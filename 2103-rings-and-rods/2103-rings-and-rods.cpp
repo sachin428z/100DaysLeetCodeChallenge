5b6f34a45c58879201932df1490e494dacb10163
Sachin <sachin3434zzz@gmail.com>
2026-06-29 19:32:07 +0530

    Time: 0 ms (100%), Space: 8.9 MB (19.24%) - LeetHub

---
+++ 2103-rings-and-rods/2103-rings-and-rods.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countPoints(string rings) {
+        int n=rings.size();
+        unordered_map<int,unordered_set<char>> mp;
+
+        for(int i=0;i<n;i+=2) {
+            char colors=rings[i];
+            int rod=rings[i+1]-'0';
+
+            mp[rod].insert(colors);
+        }
+        int count = 0;
+
+        for (auto it : mp) {
+            if (it.second.size() == 3)
+                count++;
+        }
+
+        return count;
+    }
+};
