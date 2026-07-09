c341b2c08e7f2d2913d575173171c355299417f3
Sachin <sachin3434zzz@gmail.com>
2026-07-09 11:36:12 +0530

    Time: 0 ms (100%), Space: 24 MB (24.93%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -1,69 +1,15 @@
-class SegmentTree {
-public:
-    vector<int> tree;
-
-    SegmentTree(int n) {
-        tree.resize(4 * n);
-    }
-
-    void build(vector<int>& nums, int node, int start, int end) {
-        if (start == end) {
-            tree[node] = nums[start];
-            return;
-        }
-
-        int mid = (start + end) / 2;
-        build(nums, 2 * node, start, mid);
-        build(nums, 2 * node + 1, mid + 1, end);
-
-        tree[node] = tree[2 * node] + tree[2 * node + 1];
-    }
-
-    void update(int node, int start, int end, int idx, int val) {
-        if (start == end) {
-            tree[node] = val;
-            return;
-        }
-
-        int mid = (start + end) / 2;
-
-        if (idx <= mid)
-            update(2 * node, start, mid, idx, val);
-        else
-            update(2 * node + 1, mid + 1, end, idx, val);
-
-        tree[node] = tree[2 * node] + tree[2 * node + 1];
-    }
-
-    int query(int node, int start, int end, int l, int r) {
-        if (r < start || end < l)
-            return 0;
-
-        if (l <= start && end <= r)
-            return tree[node];
-
-        int mid = (start + end) / 2;
-
-        return query(2 * node, start, mid, l, r) +
-               query(2 * node + 1, mid + 1, end, l, r);
-    }
-};
-
 class NumArray {
+    vector<int> prefix;
 public:
-    int n;
-    SegmentTree st;
-
-    NumArray(vector<int>& nums) : n(nums.size()), st(nums.size()) {
-        if (n > 0)
-            st.build(nums, 1, 0, n - 1);
+    NumArray(vector<int>& nums) {
+        prefix.resize(nums.size() + 1, 0);
+        for (int i = 0; i < nums.size(); i++)
+            prefix[i + 1] = prefix[i] + nums[i];
     }
-
-    int sumRange(int left, int right) {
-        return st.query(1, 0, n - 1, left, right);
+    int sumRange(int l, int r) {
+        return prefix[r + 1] - prefix[l];
     }
 };
-
 /**
  * Your NumArray object will be instantiated and called as such:
  * NumArray* obj = new NumArray(nums);
