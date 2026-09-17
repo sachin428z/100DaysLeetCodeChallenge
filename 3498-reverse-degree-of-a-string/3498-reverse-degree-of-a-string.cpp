7ff1319e1b9a322010cb23e00da0a3c618b833ba
Sachin Gupta <sachin3434z@gmail.com>
2026-09-17 13:53:28 +0530

    Time: 0 ms (100%), Space: 9.7 MB (48.91%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int ans=0;
+
+        for(int i=0;i<s.size();i++) {
+            int reversePos='z'-s[i]+1;
+            ans+=reversePos*(i+1);
+        }
+        return ans;
+    }
+};
